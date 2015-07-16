3ec69bb70b6d90f3db7e10ad773e2d415e5b575d
Andy McKay <amckay@mozilla.com>
2015-07-15 17:31:24 -0700

    remove localisation from concrete

--- payments_config/products.py
+++ payments_config/products.py
@@ -4,18 +4,24 @@ from utils import wrapper as _
 config = {
     # Concrete is Mozilla's example site.
     'mozilla-concrete': {
-        'name': _('Mozilla Concrete'),
+        'name': {
+            'en': 'Mozilla Concrete'
+        },
         'url': 'http://pay.dev.mozaws.net/',
         'terms': 'http://pay.dev.mozaws.net/terms/',
         'products': [
             {
                 'id': 'brick',
-                'description': _('Brick'),
+                'description': {
+                    'en': 'Brick'
+                },
                 'amount': '10.00',
             },
             {
                 'id': 'mortar',
-                'description': _('Mortar'),
+                'description': {
+                    'en': 'Mortar'
+                },
                 'amount': '5.00',
                 'img': ('https://raw.githubusercontent.com/mozilla'
                         '/payments-config/master/payments_config'
