68e533823b6a59f00f74d64e3884804eb5df442e
Andy McKay <amckay@mozilla.com>
2015-07-20 13:23:12 -0700

    Revert "remove localisation from concrete"
    This reverts commit 3ec69bb70b6d90f3db7e10ad773e2d415e5b575d.
    
    Conflicts:
    	payments_config/products.py

--- payments_config/products.py
+++ payments_config/products.py
@@ -4,25 +4,19 @@ from utils import wrapper as _
 config = {
     # Concrete is Mozilla's example site.
     'mozilla-concrete': {
-        'name': {
-            'en': 'Mozilla Concrete'
-        },
         'email': 'support@concrete.mozilla.org',
+        'name': _('Mozilla Concrete'),
         'url': 'http://pay.dev.mozaws.net/',
         'terms': 'http://pay.dev.mozaws.net/terms/',
         'products': [
             {
                 'id': 'brick',
-                'description': {
-                    'en': 'Brick'
-                },
+                'description': _('Brick'),
                 'amount': '10.00',
             },
             {
                 'id': 'mortar',
-                'description': {
-                    'en': 'Mortar'
-                },
+                'description': _('Mortar'),
                 'amount': '5.00',
                 'img': ('https://raw.githubusercontent.com/mozilla'
                         '/payments-config/master/payments_config'
